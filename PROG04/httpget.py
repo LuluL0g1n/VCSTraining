67064772747bbce7923b0965ec713c79a9a83139
LuluL0g1n <97771705+LuluL0g1n@users.noreply.github.com>
2022-10-10 23:32:19 +0700

    Update httpget.py

--- PROG04/httpget.py
+++ PROG04/httpget.py
@@ -1,8 +1,17 @@
-import socket
-request = b"GET / HTTP/1.1\nHost: blogtest.vnprogramming.com\n\n"
+import socket,argparse
+import urllib.parse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--url",dest="host")
+args = parser.parse_args()
+host = args.host
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("blogtest.vnprogramming.com", 80))
-s.send(request)
+domain = urllib.parse.urlparse(f'{host}').netloc
+s.connect((domain, 80))
+
+request = (f"GET / HTTP/1.1\r\n"+f"Host: {domain}\r\n"+f"Accept: */*\r\n"+'\r\n')
+s.send(request.encode())
 data = ''
 while not data.endswith('<title>'):
     data += s.recv(1).decode()
