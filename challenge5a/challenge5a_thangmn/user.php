af0f50f9630d950aab07cba72dca4385673807d7
LuluL0g1n <97771705+LuluL0g1n@users.noreply.github.com>
2022-11-07 10:17:33 +0700

    Update user.php

--- challenge5a/challenge5a_thangmn/user.php
+++ challenge5a/challenge5a_thangmn/user.php
@@ -10,53 +10,6 @@ class User{
         $this->$email = $user->$email;
         $this->$phone = $user->$phone;
     }
-    function setId($id){
-        $this->$id = $id;
-    }
-
-    function getId(){
-        return $this->$id;
-    }
-
-    function setUserName($username) {
-        return $this->$username = $username;
-    }
-
-    function getUserName(){
-        return $this->$username;
-    }
-
-    function setPosition($position) {
-        return $this->$position = $position;
-    }
-
-    function getPosition(){
-        return $this->$type;
-    }
-
-    function setName($name) {
-        return $this->$name = $name;
-    }
-
-    function getName(){
-        return $this->$name;
-    }
-
-    function setEmail($email) {
-        return $this->$email = $email;
-    }
-
-    function getEmail(){
-        return $this->$email;
-    }
-
-    function setPhoneNumber($phonenumber) {
-        return $this->$phonenumber = $phonenumber;
-    }
-
-    function getPhoneNumber(){
-        return $this->$phonenumber;
-    }
-
+    
 }
 ?><?php
